7c829c779c0b4c2d945e82ede37023f56d1726f0
Mayur2111995 <barimayur1265@gmail.com>
2022-07-13 20:39:09 +0530

    Added:Data Structure and Algorithm

---
+++ src/BinarySearch.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
+public class BinarySearch {
+    // Returns index of x if it is present in arr[],
+        // else return -1
+        static int binarySearch(String[] arr, String x)
+        {
+            int l = 0, r = arr.length - 1;
+            while (l <= r) {
+                int m = l + (r - l) / 2;
+
+                int res = x.compareTo(arr[m]);
+
+                // Check if x is present at mid
+                if (res == 0)
+                    return m;
+
+                // If x greater, ignore left half
+                if (res > 0)
+                    l = m + 1;
+
+                    // If x is smaller, ignore right half
+                else
+                    r = m - 1;
+            }
+
+            return -1;
+        }
+
+        public static void main(String []args)
+        {
+            String[] arr = { "Mayur", "Sachin", "Pappu", "Mayu"};
+            System.out.println("Enter a word you want to search");
+            Scanner sc = new Scanner (System.in);
+
+            String x = sc.next();
+            int result = binarySearch(arr, x);
+
+            if (result == -1)
+                System.out.println("Element not present");
+            else
+                System.out.println("Element found at "
+                        + "index " + result);
+        }
+    }
